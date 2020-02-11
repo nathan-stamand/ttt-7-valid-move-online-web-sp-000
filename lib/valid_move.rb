bcb80f59d27afad1b889c28dc8444d49c40b80bf
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-11 15:45:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,10 +3,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
-  elsif board[index] > 8
-    return false
-  elsif board[index] == nil
-  else
+  elsif !position_taken?(board, index)
     return true
   end
 end
